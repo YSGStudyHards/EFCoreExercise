feb19421b2d62b4e8a24b9cd8b9feb740951fe2b
Edwin <1070342164@qq.com>
2025-03-25 00:15:44 +0800

    Add LinqExercise

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-﻿using EFCoreExercise.DBModel;
-using Microsoft.EntityFrameworkCore;
-using System.Numerics;
-using System.Reflection;
-
-namespace EFCoreExercise
+﻿namespace EFCoreExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region LINQ中常用方法
+
+            //LinqExercise.CommonMethodsInLINQ();
+
+            #endregion
+
             #region 数据CRUD简单操作
 
-            SimpleOperation.AddData();
-            SimpleOperation.DataQueryOperation();
-            SimpleOperation.UpdateData();
-            SimpleOperation.DeleteData();
+            //SimpleOperation.AddData();
+            //SimpleOperation.DataQueryOperation();
+            //SimpleOperation.UpdateData();
+            //SimpleOperation.DeleteData();
 
             #endregion
         }
